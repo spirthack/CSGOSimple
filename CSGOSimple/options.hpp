04a73be8d40f40a777bb400cafdc5c5c19b827bc
dawn <30476099+bighaxor@users.noreply.github.com>
2017-08-04 08:29:06 -0700

    Update options.hpp

--- CSGOSimple/options.hpp
+++ CSGOSimple/options.hpp
@@ -13,49 +13,49 @@ public:
     // 
     OPTION(bool, esp_enabled, false);
     OPTION(bool, esp_enemies_only, false);
-    OPTION(bool, esp_player_boxes, true);
-    OPTION(bool, esp_player_names, true);
-    OPTION(bool, esp_player_health, true);
-    OPTION(bool, esp_player_weapons, true);
-    OPTION(bool, esp_player_snaplines, true);
-    OPTION(bool, esp_crosshair, true);
-    OPTION(bool, esp_dropped_weapons, true);
-    OPTION(bool, esp_defuse_kit, true);
-    OPTION(bool, esp_planted_c4, true);
+    OPTION(bool, esp_player_boxes, false);
+    OPTION(bool, esp_player_names, false);
+    OPTION(bool, esp_player_health, false);
+    OPTION(bool, esp_player_weapons, false);
+    OPTION(bool, esp_player_snaplines, false);
+    OPTION(bool, esp_crosshair, false);
+    OPTION(bool, esp_dropped_weapons, false);
+    OPTION(bool, esp_defuse_kit, false);
+    OPTION(bool, esp_planted_c4, false);
 
     // 
     // GLOW
     // 
     OPTION(bool, glow_enabled, false);
     OPTION(bool, glow_enemies_only, false);
-    OPTION(bool, glow_players, true);
-    OPTION(bool, glow_chickens, true);
-    OPTION(bool, glow_c4_carrier, true);
-    OPTION(bool, glow_planted_c4, true);
-    OPTION(bool, glow_defuse_kits, true);
-    OPTION(bool, glow_weapons, true);
+    OPTION(bool, glow_players, false);
+    OPTION(bool, glow_chickens, false);
+    OPTION(bool, glow_c4_carrier, false);
+    OPTION(bool, glow_planted_c4, false);
+    OPTION(bool, glow_defuse_kits, false);
+    OPTION(bool, glow_weapons, false);
 
     //
     // CHAMS
     //
     OPTION(bool, chams_player_enabled, false);
     OPTION(bool, chams_player_enemies_only, false);
-    OPTION(bool, chams_player_wireframe, true);
-    OPTION(bool, chams_player_flat, true);
-    OPTION(bool, chams_player_ignorez, true);
+    OPTION(bool, chams_player_wireframe, false);
+    OPTION(bool, chams_player_flat, false);
+    OPTION(bool, chams_player_ignorez, false);
     OPTION(bool, chams_player_glass, false);
-    OPTION(bool, chams_arms_enabled, true);
+    OPTION(bool, chams_arms_enabled, false);
     OPTION(bool, chams_arms_wireframe, false);
     OPTION(bool, chams_arms_flat, false);
     OPTION(bool, chams_arms_ignorez, false);
-    OPTION(bool, chams_arms_glass, true);
+    OPTION(bool, chams_arms_glass, false);
 
     //
     // MISC
     //
-    OPTION(bool, misc_bhop, true);
+    OPTION(bool, misc_bhop, false);
     OPTION(bool, misc_no_hands, false);
-    OPTION(int, viewmodel_fov, 90);
+    OPTION(int, viewmodel_fov, 68);
     OPTION(float, mat_ambient_light_r, 0.0f);
     OPTION(float, mat_ambient_light_g, 0.0f);
     OPTION(float, mat_ambient_light_b, 0.0f);
