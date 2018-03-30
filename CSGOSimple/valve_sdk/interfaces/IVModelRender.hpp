fc07d059be427bf225e53065112577e2d1d7b011
a47 <slava@volnoj.ingrii>
2018-03-30 17:21:58 +0300

    csgo updated; interface fix

--- CSGOSimple/valve_sdk/interfaces/IVModelRender.hpp
+++ CSGOSimple/valve_sdk/interfaces/IVModelRender.hpp
@@ -96,6 +96,7 @@ struct ModelRenderInfo_t
 {
     Vector                  origin;
     QAngle                  angles;
+	char                    pad[4];
     IClientRenderable*      pRenderable;
     const model_t*          pModel;
     const matrix3x4_t*      pModelToWorld;
