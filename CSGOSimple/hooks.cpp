b7f92d261691d345b5ea2697d6b8055226ae971d
a47 <anarh1st47@pm.me>
2019-09-17 17:09:12 +0300

    serverv browser fix + ground texture extended fix(thanks @rraggerr)

--- CSGOSimple/hooks.cpp
+++ CSGOSimple/hooks.cpp
@@ -72,6 +72,9 @@ namespace Hooks {
 		crosshair_cvar->SetValue(!(g_Options.esp_enabled && g_Options.esp_crosshair));
 
 		DWORD colorwrite, srgbwrite;
+		IDirect3DVertexDeclaration9* vert_dec = nullptr;
+		IDirect3DVertexShader9* vert_shader = nullptr;
+		DWORD dwOld_D3DRS_COLORWRITEENABLE = NULL;
 		pDevice->GetRenderState(D3DRS_COLORWRITEENABLE, &colorwrite);
 		pDevice->GetRenderState(D3DRS_SRGBWRITEENABLE, &srgbwrite);
 
@@ -79,6 +82,11 @@ namespace Hooks {
 		//removes the source engine color correction
 		pDevice->SetRenderState(D3DRS_SRGBWRITEENABLE, false);
 
+		pDevice->GetRenderState(D3DRS_COLORWRITEENABLE, &dwOld_D3DRS_COLORWRITEENABLE);
+		pDevice->GetVertexDeclaration(&vert_dec);
+		pDevice->GetVertexShader(&vert_shader);
+		pDevice->SetRenderState(D3DRS_COLORWRITEENABLE, 0xffffffff);
+		pDevice->SetRenderState(D3DRS_SRGBWRITEENABLE, false);
 		pDevice->SetSamplerState(NULL, D3DSAMP_ADDRESSU, D3DTADDRESS_WRAP);
 		pDevice->SetSamplerState(NULL, D3DSAMP_ADDRESSV, D3DTADDRESS_WRAP);
 		pDevice->SetSamplerState(NULL, D3DSAMP_ADDRESSW, D3DTADDRESS_WRAP);
@@ -90,11 +98,7 @@ namespace Hooks {
 		ImGui::NewFrame();
 
 
-		ImDrawList* esp_drawlist = nullptr;
-
-	
-
-		esp_drawlist = Render::Get().RenderScene();
+		auto esp_drawlist = Render::Get().RenderScene();
 
 		Menu::Get().Render();
 	
@@ -105,6 +109,10 @@ namespace Hooks {
 
 		pDevice->SetRenderState(D3DRS_COLORWRITEENABLE, colorwrite);
 		pDevice->SetRenderState(D3DRS_SRGBWRITEENABLE, srgbwrite);
+		pDevice->SetRenderState(D3DRS_COLORWRITEENABLE, dwOld_D3DRS_COLORWRITEENABLE);
+		pDevice->SetRenderState(D3DRS_SRGBWRITEENABLE, true);
+		pDevice->SetVertexDeclaration(vert_dec);
+		pDevice->SetVertexShader(vert_shader);
 
 		return oEndScene(pDevice);
 	}
