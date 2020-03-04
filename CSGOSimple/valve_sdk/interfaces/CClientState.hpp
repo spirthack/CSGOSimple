dfa52994d83bc81c2942dd4e3770d52df7a5e69a
hvhbot <31180339+hvhbot@users.noreply.github.com>
2020-03-04 22:21:30 +0300

    Update CClientState.hpp
    лично в реклассе чекал и в чите юзаю полёт нормальный👍🏻

--- CSGOSimple/valve_sdk/interfaces/CClientState.hpp
+++ CSGOSimple/valve_sdk/interfaces/CClientState.hpp
@@ -44,29 +44,45 @@ public:
         m_nDeltaTick = -1;
     }
 
-    char pad_0000[156];             //0x0000
-    uint32_t m_NetChannel;          //0x009C
-    uint32_t m_nChallengeNr;        //0x00A0
-    char pad_00A4[100];             //0x00A4
-    uint32_t m_nSignonState;        //0x0108
-    char pad_010C[8];               //0x010C
-    float m_flNextCmdTime;          //0x0114
-    uint32_t m_nServerCount;        //0x0118
-    uint32_t m_nCurrentSequence;    //0x011C
-    char pad_0120[8];               //0x0120
-    CClockDriftMgr m_ClockDriftMgr; //0x0128
-    uint32_t m_nDeltaTick;          //0x0178
-    bool m_bPaused;                 //0x017C
-    char pad_017D[3];               //0x017D
-    uint32_t m_nViewEntity;         //0x0180
-    uint32_t m_nPlayerSlot;         //0x0184
-    char m_szLevelName[260];        //0x0188
-    char m_szLevelNameShort[40];    //0x028C
-    char m_szGroupName[40];         //0x02B4
-    char pad_02DC[56];              //0x02DC
-    uint32_t m_nMaxClients;         //0x0310
-    char pad_0314[18940];           //0x0314
-    Vector viewangles;              //0x4D10
+   	char pad_0000[156]; //0x0000
+	INetChannel* m_NetChannel; //0x0094
+	//char pad_0098[8]; //0x0098
+	uint32_t m_nChallengeNr; //0x00A0
+	char pad_00A4[100]; //0x00A4
+	uint32_t m_nSignonState; //0x0108
+	char pad_010C[8]; //0x010C
+	float m_flNextCmdTime; //0x0114
+	uint32_t m_nServerCount; //0x0118
+	uint32_t m_nCurrentSequence; //0x011C
+	char pad_0120[8];
+	CClockDriftMgr clock_drift_mgr; //0x0128
+	uint32_t m_nDeltaTick; //0x0174
+	bool m_bPaused; //0x0178
+	char pad_0179[3]; //0x0179
+	uint32_t m_nViewEntity; //0x0180
+	uint32_t m_nPlayerSlot; //0x0184
+	char m_szLevelName[260]; //0x0188
+	char m_szLevelNameShort[40]; //0x028C
+	char pad_01479[40]; //0x02BC
+	char m_szGroupName[40]; //0x02B4
+	char pad_02DC[136]; //0x02DC
+	uint32_t m_nMaxClients; //0x0310
+	char pad_0314[18820]; //0x0314
+	float m_flLastServerTickTime; //0x4C98
+	bool insimulation; //0x4C9C
+	char pad_4C9D[3]; //0x4C9D
+	uint32_t oldtickcount; //0x4CA0
+	float m_tickRemainder; //0x4CA4
+	float m_frameTime; //0x4CA8
+	uint32_t lastoutgoingcommand; //0x4CAC
+	uint32_t chokedcommands; //0x4CB0 4D28
+	uint32_t last_command_ack; //0x4CB4
+	uint32_t command_ack; //0x4CB8
+	uint32_t m_nSoundSequence; //0x4CBC
+	char pad_4CC0[80]; //0x4CC0
+	QAngle viewangles; //0x4D88
+	char pad_4D1C[0xD0]; //0x4D1C
+	CEventInfo* events; //0x4DEC
 }; //Size: 0x4D1C
 
 #pragma pack(pop)
@@ -74,5 +90,6 @@ public:
 static_assert(FIELD_OFFSET(CClientState, m_NetChannel)       == 0x009C, "Wrong struct offset");
 static_assert(FIELD_OFFSET(CClientState, m_nCurrentSequence) == 0x011C, "Wrong struct offset");
 static_assert(FIELD_OFFSET(CClientState, m_nDeltaTick)       == 0x0174, "Wrong struct offset");
-static_assert(FIELD_OFFSET(CClientState, m_nMaxClients)      == 0x0310, "Wrong struct offset");
-static_assert(FIELD_OFFSET(CClientState, viewangles)         == 0x4D10, "Wrong struct offset");
+static_assert(FIELD_OFFSET(CClientState, m_nMaxClients) == 0x388, "Wrong struct offset");
+static_assert(FIELD_OFFSET(CClientState, chokedcommands) == 0x4D28, "Wrong struct offset");
+static_assert(FIELD_OFFSET(CClientState, viewangles) == 0x4D88, "Wrong struct offset");
