0fdaefc46003d40669e0fb144eaa9ff316f38a27
kirill <63228361+kirill-21@users.noreply.github.com>
2022-01-19 16:51:10 +0300

    Updated CInput struct

--- CSGOSimple/valve_sdk/interfaces/CInput.hpp
+++ CSGOSimple/valve_sdk/interfaces/CInput.hpp
@@ -10,51 +10,37 @@ class bf_read;
 class CInput
 {
 public:
-    char                pad_0000[12];
-    bool                m_fTrackIRAvailable;
-    bool                m_fMouseInitialized;
-    bool                m_fMouseActive;
-    bool                m_fJoystickAdvancedInit;
-    char                pad_0010[44];
-    char*               m_pKeys;
-    char                pad_0040[48];
-    int32_t             m_nCamCommand;
-    char                pad_0074[76];
-    bool                m_fCameraInterceptingMouse;
-    bool                m_fCameraInThirdPerson;
-    bool                m_fCameraMovingWithMouse;
-    char                pad_00C3[1];
-    Vector              m_vecCameraOffset;
-    bool                m_fCameraDistanceMove;
-    char                pad_00D1[19];
-    bool                m_CameraIsOrthographic;
-    bool                m_CameraIsThirdPersonOverview;
-    char                pad_00E6[2];
-    QAngle*             m_angPreviousViewAngles;
-    QAngle*             m_angPreviousViewAnglesTilt;
-    char                pad_00F0[16];
-    float               m_flLastForwardMove;
-    int32_t             m_nClearInputState;
+    std::byte			pad0[0xC];
+    bool				m_fTrackIRAvailable;
+    bool				m_fMouseInitialized;
+    bool				m_fMouseActive;
+    std::byte			pad1[0x9E];
+    bool				m_fCameraInThirdPerson;
+    std::byte			pad2[0x2];
+    Vector				m_vecCameraOffset;
+    std::byte			pad3[0x38];
     CUserCmd*           m_pCommands;
     CVerifiedUserCmd*   m_pVerifiedCommands;
 
-	inline CUserCmd* GetUserCmd(int sequence_number);
-	inline CUserCmd * GetUserCmd(int nSlot, int sequence_number);
-	inline CVerifiedUserCmd* GetVerifiedCmd(int sequence_number);
+    inline CUserCmd* GetUserCmd(const int sequence_number);
+    inline CUserCmd* GetUserCmd(int nSlot, int sequence_number);
+    inline CVerifiedUserCmd* GetVerifiedCmd(const int sequence_number);
 };
+static_assert(sizeof(CInput) == 0xFC);
+
 
 CUserCmd* CInput::GetUserCmd(const int sequence_number)
 {
-    return &(*(CUserCmd**)((DWORD)this + 0x0108))[sequence_number % MULTIPLAYER_BACKUP];
+    return &m_pCommands[sequence_number % MULTIPLAYER_BACKUP];
 }
 
-CUserCmd *CInput::GetUserCmd(int nSlot, const int sequence_number)
+CUserCmd* CInput::GetUserCmd(int nSlot, const int sequence_number)
 {
-	typedef CUserCmd*(__thiscall *GetUserCmd_t)(void*, int, int);
-	return CallVFunction<GetUserCmd_t>(this, 8)(this, nSlot, sequence_number);
+    typedef CUserCmd* (__thiscall* GetUserCmd_t)(void*, int, int);
+    return CallVFunction<GetUserCmd_t>(this, 8)(this, nSlot, sequence_number);
 }
 
 CVerifiedUserCmd* CInput::GetVerifiedCmd(const int sequence_number)
 {
-    return &(*(CVerifiedUserCmd**)((DWORD)this + 0x010C))[sequence_number % MULTIPLAYER_BACKUP];
+    return &m_pVerifiedCommands[sequence_number % MULTIPLAYER_BACKUP];
 }
